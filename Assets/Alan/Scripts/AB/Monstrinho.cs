c2a4106a07a9ec5a5bad2366a465769e1f686bf6
Thomas Redekop <redetho@gmail.com>
2021-11-11 17:08:25 -0300

    mudanças na arvore

--- Assets/Alan/Scripts/AB/Monstrinho.cs
+++ Assets/Alan/Scripts/AB/Monstrinho.cs
@@ -2,18 +2,24 @@ using UnityEngine;
 
 public class Monstrinho : SpawnerManager
 {
-    public int dano = 5;
+    public int dano = 1;
     public float moveSpeed = 5f;
     private GameObject Player;
-    private MyHealthSystem PlayerHP;
+    public MyHealthSystem PlayerHP;
     private Vector2 movement;
     private Rigidbody2D rb;
+            EnemiesScript enemy;
 
     private void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
 
         Player = GameObject.FindGameObjectWithTag("Player");
+
+        PlayerHP = MyHealthSystem.FindObjectOfType<MyHealthSystem>();
+
+        enemy = GetComponent<EnemiesScript>();
+
     }
 
     private void Update()
@@ -25,11 +31,14 @@ public class Monstrinho : SpawnerManager
         rb.velocity = direction*moveSpeed;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.collider.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
-            Player.GetComponent<MyHealthSystem>().Dano(dano);
+            PlayerHP.Dano(dano);
+            enemy.TakeDamage(enemy.maxHealth);
+            Destroy(this.gameObject);
+
         }
     }
 }
--- Assets/Alan/Scripts/AB/Platform.cs
+++ Assets/Alan/Scripts/AB/Platform.cs
@@ -7,6 +7,9 @@ public class Platform : MonoBehaviour
     public Transform pos1, pos2;
     public float speed;
     public Transform startPos;
+    public Transform parent;
+    public GameObject player;
+    public Transform managert;
 
     Vector3 nextPos;
 
@@ -27,5 +30,21 @@ public class Platform : MonoBehaviour
         }
 
         transform.position = Vector3.MoveTowards(transform.position, nextPos, speed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            player.transform.SetParent(parent);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            player.transform.SetParent(managert);
+        }
     }
 }
