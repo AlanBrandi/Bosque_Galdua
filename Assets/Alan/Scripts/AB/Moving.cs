f2bb1c352e1cbfd94205975a61b86b9fd7e220ec
Frederico Proença <joopereight@gmail.com>
2021-11-11 14:27:09 -0300

    Som de passo colocado, colocados inimigos nas cenas, mudado tempo de respawn do monstro voador, mudado colisão do mesmo.

--- Assets/Alan/Scripts/AB/Moving.cs
+++ Assets/Alan/Scripts/AB/Moving.cs
@@ -9,8 +9,9 @@ public class Moving : MonoBehaviour
     public Rigidbody2D rb;
     public float hor;
     public float speed = 1;
-
-    
+    bool isMoving = false;
+    public AudioSource footstep;
+    public Jumping godpleasehelpme;
     private void FixedUpdate()
     {
         hor = Input.GetAxisRaw("Horizontal");
@@ -26,5 +27,27 @@ public class Moving : MonoBehaviour
         }
 
         MyAnimator.SetFloat("Speed", rb.velocity.magnitude);
+
+        float randomPitch = Random.Range(0.8f, 1.2f);
+        footstep.pitch = randomPitch;
+
+        if (rb.velocity.x !=0 && godpleasehelpme.IsGrounded == true)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
+
+        if (isMoving)
+        {
+            if (!footstep.isPlaying)
+                footstep.Play();
+        }
+        else
+        {
+            footstep.Stop();
+        }
     }
 }
