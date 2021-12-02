589e81a24c22d7684e5da0c23e29f2c0b2945bf5
Frederico Proença <joopereight@gmail.com>
2021-12-02 17:38:18 -0300

    Vida, texto.
    Consertado a vida para fazer com que ao sair e entrar no jogo ela volte ao valor original.
    Feito com que o texto de loading tenha ancoragem no canto inferior direito da tela.

--- Assets/FPSLimiter.cs
+++ Assets/FPSLimiter.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FPSLimiter : MonoBehaviour
-{    void Awake()
+{
+    public GameData_SO gameData;
+    Scene sceneName;
+    string forest = "Forest";
+    void Awake()
     {
+        sceneName = SceneManager.GetActiveScene();
+        if (sceneName.name == forest)
+        {
+            gameData.lives = 15;
+        }
         QualitySettings.vSyncCount = 0;  // VSync must be disabled
         Application.targetFrameRate = 60;
     }
