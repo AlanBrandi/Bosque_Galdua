0ee2d0a33e670f928d6a9ead870cefaeb32032fb
M0kiss <alan.brandi@hotmail.com>
2022-03-18 18:27:18 -0300

    objeto novo

--- Assets/Alan/Scripts/AB/MakeItAObject.cs
+++ Assets/Alan/Scripts/AB/MakeItAObject.cs
@@ -9,12 +9,14 @@ public class MakeItAObject : MonoBehaviour
     #region BasicComponents
     GameObject MyGOB;
     GameObject Player;
+    ObjectScript objectScript;
 
 
     private void Awake()
     {
         MyGOB = this.gameObject;
         Player = GameObject.Find("PlayerManager");
+        objectScript = Player.GetComponent<ObjectScript>();
     }
     private void Start()
     {
@@ -24,18 +26,22 @@ public class MakeItAObject : MonoBehaviour
     }
     #endregion
 
-    public GameObject HitFx;
+    //public GameObject HitFx;
     public GameObject ExplodeFx;
     public int AttackDamage;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.tag == "Enemies")
+        if (collision.collider.tag == "Enemies" && objectScript.pegou != true)
         {
             collision.collider.GetComponent<EnemiesScript>().TakeDamage(AttackDamage);
         }
+        else if(collision.collider.tag == "Enemies" && objectScript.pegou == true)
+        {
+            Destruir();
+        }
 
-        if (collision.collider.tag != "Player" && Player.GetComponent<ObjectScript>().pegou == false && Player.GetComponent<ObjectScript>().pegouNum > 0)
+        if (collision.collider.tag != "Player" && Player.GetComponent<ObjectScript>().pegou == false && objectScript.pegouNum > 0)
         {
             Destruir();
             //dar dano no bixo
@@ -47,5 +53,6 @@ public class MakeItAObject : MonoBehaviour
        // Instantiate(HitFx, positionHit, Quaternion.identity);
         Instantiate(ExplodeFx, MyGOB.transform.position, Quaternion.identity);
         Destroy(MyGOB);
+        objectScript.pegouNum = 0;
     }
 }
