78ac934f7544c8370bc1e301ea64fcf7e775017e
Thiago Fontoura <thiagoatf@hotmail.com>
2022-06-13 04:31:47 -0300

    Revert "Boss Part1"
    This reverts commit 4049847371812e9cc0bee4682d1d132921193733.

--- Assets/Thiago/Scripts/Boss.cs
+++ Assets/Thiago/Scripts/Boss.cs
@@ -1,74 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class Boss : MonoBehaviour
 {
-
-    
-    public Slider healthBar;
-    public int currentHealth;
-
-    GameObject FireAttack;
+    public GameObject FireAttack;
     public GameObject shockWaveAttack;
     public GameObject summonAttack;
-    public GameObject summonAttack1;
     public Transform summonAttackPos;
-    public Transform summonAttackPos1;
-    public Transform summonAttackPos2;
-    Transform fireGroundPos;
+    public Transform fireGroundPos;
     public Transform shockwavePos;
     public float attackrate;
     int randomState = 0;
-     acidSpawn acid;
+    public acidSpawn acid;
     float nextAttack = 0f;
     public float acidRate;
     float nextAcid = 0;
-    public Animator anim;
-    public bool IsIdle = true;
-    int chooseStatee;
-
-    public MyHealthSystem PlayerHP;
-
-    int contS = 0;
-    int contA = 0;
-    int contL = 0;
 
 
     private void Start()
     {
         acid = acid.GetComponent<acidSpawn>();
-        anim = this.GetComponent<Animator>();
     }
 
     private void Update()
     {
-        healthBar.value = currentHealth;
-        if (Time.time > nextAttack)
+        if(Time.time > nextAttack)
         {
           
                 nextAttack = Time.time + attackrate;
-            if(contA >= 2)
-            {
-                chooseStatee = 2;
-                chooseState();
-            }
-            else if(contL >= 2)
-            {
-                chooseStatee = 0;
-                chooseState();
-            }
-            else if(contS >= 2)
-            {
-                chooseStatee = 1;
-                chooseState();
-            }
-            else if(contA < 2 && contL < 2 && contS < 2)
-            {
                 RandomState();
-            }
-                
                       
         }
         if (Time.time > nextAcid && randomState == 3)
@@ -77,31 +38,16 @@ public class Boss : MonoBehaviour
             nextAcid = Time.time + acidRate;
             acidRain();
         }
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            TakeDamage(5);
-        }
     }
 
-    public void summonAnim()
-    {
-        Debug.Log("SUMMON ATTACK");
-        anim.SetTrigger("Summon");
-    }
     public void summon()
     {
-        Instantiate(summonAttack1, summonAttackPos.transform.position, Quaternion.identity);
-        Instantiate(summonAttack, summonAttackPos1.transform.position, Quaternion.identity);
-        Instantiate(summonAttack1, summonAttackPos2.transform.position, Quaternion.identity);
-        summonAttack.transform.rotation = Quaternion.Euler(0, 180, 0);
-    }
-    public void shockWaveAnim()
-    {
-        Debug.Log("SHOCK WAVE ATTACK");   
-        anim.SetTrigger("AttackWave");
+        Debug.Log("SUMMON ATTACK");
+       // Instantiate(summonAttack, summonAttackPos.transform.position, Quaternion.identity);
     }
     public void shockWave()
     {
+        Debug.Log("SHOCK WAVE ATTACK");
         Instantiate(shockWaveAttack, shockwavePos.transform.position, Quaternion.identity);
     }
     public void fireGroundAttack()
@@ -118,98 +64,29 @@ public class Boss : MonoBehaviour
         acid.acidgo = true;
        
     }
-    public void laser()
+    public void lazer()
     {
         Debug.Log("LAZER ATTACK");
-        anim.SetTrigger("Laser");
-    }
-
-    public void TakeDamage(int damage)
-    {
-        
-            currentHealth -= damage;
-            if (currentHealth > 0)
-            {
-                anim.SetTrigger("Hurt");
-            }
-            Debug.Log("Damage!");
-        
-        
-
-        if (currentHealth <= 0)
-        {
-            anim.SetTrigger("Die");
-        }
-
     }
     public void RandomState()
     {
-        IsIdle = false;
-        randomState = Random.Range(0, 3);
+        randomState = Random.Range(0, 4);
         if (randomState == 0)
         {
-            contS++;
-            contA = 0;
-            contL = 0;
-            attackrate = 10;
-            summonAnim();
+            summon();
         }
         else if (randomState == 1)
         {
-            contA++;
-            contS = 0;
-            contL = 0;
-            attackrate = 10;
-            shockWaveAnim();
+            shockWave();
         }
         else if (randomState == 2)
         {
-            contL++;
-            contS = 0;
-            contA = 0;
-            attackrate = 5;
-            laser();
+            fireGroundAttack();
         }
-
-        
-
-    }
-    public void chooseState()
-    {
-        
-        if (chooseStatee == 0)
+        else if (randomState == 3)
         {
-            contS++;
-            contA = 0;
-            contL = 0;
-            attackrate = 10;
-            summonAnim();
-        }
-        else if (chooseStatee == 1)
-        {
-            contA++;
-            contS = 0;
-            contL = 0;
-            attackrate = 10;
-            shockWaveAnim();
-        }
-        else if (chooseStatee == 2)
-        {
-            contL++;
-            contS = 0;
-            contA = 0;
-            attackrate = 5;
-            laser();
+            Debug.Log("ACID RAIN");
+            acidRain();
         }
     }
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.CompareTag("Player"))
-        {
-            PlayerHP.Dano(2);
-            
-            
-        }
-    }
-
 }
