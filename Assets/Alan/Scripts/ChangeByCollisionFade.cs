ea025d55c801f02028b069fbc4d91573cf8d5cc0
AlanBrandi <alan.brandi@hotmail.com>
2023-06-20 02:19:54 -0300

    PlayerCutscene cavernas integration

--- Assets/Alan/Scripts/ChangeByCollisionFade.cs
+++ Assets/Alan/Scripts/ChangeByCollisionFade.cs
@@ -1,8 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ChangeByCollisionFade : MonoBehaviour
 {
-   // [SerializeField] Fade
+   [SerializeField] private LevelChanger levelChanger;
+
+   private void OnTriggerEnter2D(Collider2D other)
+   {
+      if(other.CompareTag("Player"))
+      {
+         Debug.Log("Entrei uai");
+         levelChanger.FadeToLevel("PlayerFallCutscene");
+      }
+   }
 }
